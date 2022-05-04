f503b915cd31aafa04791e47b01b1061e4e91603
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-05-04 17:38:55 +0200

    header added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -11,7 +11,16 @@
                 </li>
             </ul> --}}
             <ul>
-                <li>Ciao</li>
+                <li><a href="#">characters</a></li>
+                <li><a href="#">comics</a></li>
+                <li><a href="#">movies</a></li>
+                <li><a href="#">tv</a></li>
+                <li><a href="#">v</a></li>
+                <li><a href="#">collectibles</a></li>
+                <li><a href="#">videos</a></li>
+                <li><a href="#">fans</a></li>
+                <li><a href="#">news</a></li>
+                <li><a href="#">shop</a></li>
             </ul>
         </nav>
     </div>
