cde0133f5e13730dc634b2e36085211486f18b4f
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-05-05 19:13:23 +0200

    style css/ boostrap added

--- resources/views/guest/comics-detail.blade.php
+++ resources/views/guest/comics-detail.blade.php
@@ -8,20 +8,152 @@
 @endsection --}}
 @section( 'main-content')
 
+<div class="container-fluid bg-primary banner">
+    <div class="container-banner ">
+        <img src="{{ $comicsBook['thumb']}}" alt="{{ $comicsBook['title']}}">
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container-top position-relative">
+        <div class="row">
+            <div class="col-9">
+                <h2> {{$comicsBook['title']}}</h2>
+                <div class="row">
+                    <div class="col-8 d-flex justify-content-between">
+                        <p>U.S. Price {{$comicsBook['price']}}</p>
+                        <p>AVAILABLE</p>
+                    </div>
+                    <div class="col-4 text-end">
+                        <p>
+                            Check Avaiability &#9662;
+                        </p>
+                    </div>
+                </div>
+                <p>
+                    {{$comicsBook['description']}}
+                </p>
 
-<div class="comics-book-container">
+            </div>
+            <div class="col-3">
+                <p class="text-uppercase text-end"> adv</p>
+                <img  class="img-fluid" src="{{asset("images/adv.jpg")}}" alt="">
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid bg-comics-book">
+    <div class="container-bottom">
+        <div class="row">
+            <div class="col-6">
+                <h3>
+                    Talent
+                </h3>
+                <div class="row">
+                    <div class="col-4">
+                        <h5>Art by :</h5>
+                    </div>
+                    <div class="col-8">
+                        <p>
+                            @foreach ($comicsBook['artists'] as $artist)
+                            <span>{{$artist}}</span>,
+                            @endforeach
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-4">
+                        <h5>Written by : </h5>
+                    </div>
+                    <div class="col-8">
+                        <p>
+                            @foreach ($comicsBook['writers'] as $writer)
+                            <span>{{$writer}}</span>
+                            @endforeach
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">
+                <h3>
+                    Specs
+                </h3>
+                <div class="row">
+                    <div class="col-4 ">
+                        <h5>Series: </h5>
+                    </div>
+                    <div class="col-8 text-uppercase">
+                        <p>{{$comicsBook['series']}}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-4">
+                        <h5>U.S. Price :</h5>
+                    </div>
+                    <div class="col-8">
+                        <p>{{$comicsBook['price']}}</p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-4">
+                        <h5> On Sale Date: </h5>
+                    </div>
+                    <div class="col-8">
+                        <p>{{$comicsBook['sale_date']}}</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <div class="container-icons">
+        <div class="comics-book-bottom">
+            <ul>
+                <li>
+                    <img src="{{ asset('images/buy-comics-digital-comics.png') }}" alt="">
+                    <span>
+                        Digital comics
+                    </span>
+                </li>
+                <li>
+                    <img src="{{ asset('images/buy-comics-merchandise.png') }}" alt="">
+                    <span>
+                        dc merchandising
+                    </span></li>
+                <li>
+                    <img src="{{ asset('images/buy-comics-subscriptions.png') }}" alt="">
+                        <span>
+                            subscription
+                        </span></li>
+                <li><img src="{{ asset('images/buy-comics-shop-locator.png') }}" alt="">
+                    <span>
+                        comic shop locator
+                    </span></li>
+                <li>
+                    <img src="{{ asset('images/buy-dc-power-visa.svg') }}" alt="">
+                    <span>
+                        DC power visa
+                    </span>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+{{-- <div class="comics-book-container">
     <div class="banner"></div>
     <div class="comics-book-cover">
         <img src="{{ $comicsBook['thumb']}}" alt="{{ $comicsBook['title']}}">
         <div class=comics-book-txt>
-            <h2> {{$comicsBook['title']}}</h2> {{-- uppercase  --}}
+            <h2> {{$comicsBook['title']}}</h2>
             <div class="comics-book-info">
-                <div class="comics-book-price">{{-- d-flex j-space between border right --}}
+                <div class="comics-book-price">
                     <p>U.S. Price {{$comicsBook['price']}}</p>
                     <p>AVAILABLE</p>
                 </div>
-                <div class="comics-book-avaiability"> {{-- check avaiability --}}
-                        <p>im a fking select</p>
+                <div class="comics-book-avaiability">
+
                 </div>
             </div>
             <p>
@@ -29,6 +161,9 @@
             </p>
         </div>
     </div>
+
+
+
     <div class="comics-book-detail clearfix">
         <div class="talent">
             <h2>Talent</h2>
@@ -96,6 +231,6 @@
         </ul>
     </div>
 
-</div>
+</div> --}}
 
 @endsection
