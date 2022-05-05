257f5c522100c0bd73fcef356ba27727556e761d
Manuel Gerolin <manuel31gerolin@gmail.com>
2022-05-05 16:20:14 +0200

    css/scss improvement

--- resources/views/comics-detail.blade.php
+++ resources/views/comics-detail.blade.php
@@ -11,10 +11,29 @@
 
 <div class="comics-book-container">
     <div class="banner"></div>
-        <div class="comics-book-detail">
-            <img src="{{ $comicsBook['thumb']}}" alt="{{ $comicsBook['title']}}">
-            <h4> {{$comicsBook['title']}}</h4>
+    <div class="comics-book-cover">
+        <img src="{{ $comicsBook['thumb']}}" alt="{{ $comicsBook['title']}}">
+        <div class=comics-book-txt>
+            <h2> {{$comicsBook['title']}}</h2> {{-- uppercase  --}}
+            <div class="comics-book-info">
+                <div class="comics-book-price">{{-- d-flex j-space between border right --}}
+                    <p>U.S. Price {{$comicsBook['price']}}</p>
+                    <p>AVAILABLE</p>
+                </div>
+                <div class="comics-book-avaiability"> {{-- check avaiability --}}
+                        <p>im a fking select</p>
+                </div>
+            </div>
+            <p>
+                {{$comicsBook['description']}}
+            </p>
         </div>
+    </div>
+    <div class="comics-book-detail clearfix">
+        <div>talent</div>
+        <div>speces</div>
+    </div>
+
 </div>
 
 @endsection
